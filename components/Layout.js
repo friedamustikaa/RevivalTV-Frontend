559a4fc771e99076e0f9ab2c9eb7b76ddf320086
Frieda Mustika <frieda@amalan.com>
2021-09-23 16:33:08 +0700

    feat(): create feature section on landing page

--- components/Layout.js
+++ components/Layout.js
@@ -6,11 +6,7 @@ export default function Layout(props) {
     <div>
       <Head>
         <title>{props.title}</title>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          crossOrigin="true"
-        />
+        
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
       {props.children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
     extend: {
       fontFamily:{
-        'poppins': ['Poppins', 'sans-serif'],
+        'poppins': ['Poppins'],
+      },
+      fontSize: {
+        'xs':'16px',
+        'sm':'18px',
+        'tiny':'20px',
+        'base':'24px',
+        'lg':'26px',
+        'xl':'28px',
+        '2xl':'36px',
+        '3xl':'48px',
+        '4xl':'64px',
       },
       fontWeight: {
-        thin: 100,
-        extralight: 200,
-        light: 300,
-        normal: 400,
-        medium: 500,
-        semibold: 600,
-        bold: 700,
-        extrabold: 800,
-        black: 900,
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
       },
       container: {
         center: true,
-        padding: '10rem'
+        // padding: '10rem'
       },
       backgroundImage: {
-        hero: "url('/assets/Hero.svg')"
+        hero: "url('/assets/Hero.svg')",
       }
     },
   },
